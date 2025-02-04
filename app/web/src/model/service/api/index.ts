a644db8cb278e108bb6ed9b1bc7c4f59407f6e95
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-02-04 17:21:12 +0800

    feat: service inject to angular application-root and MVC core.

---
+++ app/web/src/model/service/api/index.ts
@@ -0,0 +1,19 @@
+// Import Libraries
+import MVC from "@/framework/pattern/facade/mvc";
+import { Service } from "@/framework/pattern/proxy";
+import { Injectable } from '@angular/core';
+
+// Declare class
+@Injectable({
+  providedIn: 'root'
+})
+class APIService extends Service {}
+    constructor() {
+        super();
+        if (!MVC.model.has(this.name)) MVC.register(this);
+    }
+
+    msg(message: string) {
+        console.log(`---${message}---`)
+    }
+}
