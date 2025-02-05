48bd84338cb12797fb7fdea451025d3faf5e1cec
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-02-05 11:54:55 +0800

    feat: add view/layer

---
+++ app/web/src/view/layer/content/index.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'layer-content',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
+})
+export class ContentLayer {}
---
+++ app/web/src/view/layer/foreground/index.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'layer-foreground',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
+})
+export class ForgroundLayer {}
