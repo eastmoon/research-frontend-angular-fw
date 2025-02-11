dcca0896c0a42714a57b84617f3be7257c51faf8
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2025-02-11 14:33:46 +0800

    feat: add lazy loading component

---
+++ app/web/src/view/component/lazy-e/index.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [CommonModule],
+  selector: 'app-component-lazy-e',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
+})
+export class LazyEComponent {
+
+}
